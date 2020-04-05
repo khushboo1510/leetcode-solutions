e68863aa7975425afa4705af73cb0b3ed8188723
khushboo15 <khushboomehta1995@gmail.com>
2020-04-05 01:19:55 -0400

    Problem 1. Solved in O(n). Find element that has not been repeated in an array.

---
+++ 30-Day LeetCoding Challenge/April/1. Single Number.py
@@ -0,0 +1,14 @@
+"""
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+"""
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        a = 0
+        for i in nums:
+            a ^= i
+        return a
