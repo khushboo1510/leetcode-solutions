aa28856b7613d2f32ccfd08c0e5ecf123586a962
khushboo1510 <khushboomehta1995@gmail.com>
2020-04-26 19:48:18 -0400

    Solved in O(n)

--- Easy/819. Most Common Word.py
+++ Easy/819. Most Common Word.py
@@ -1,13 +1,19 @@
+"""
+https://leetcode.com/problems/most-common-word/
+
+Given a paragraph and a list of banned words, return the most frequent word that is not in the list of banned words.  It is guaranteed there is at least one word that isn't banned, and that the answer is unique.
+
+Words in the list of banned words are given in lowercase, and free of punctuation.  Words in the paragraph are not case sensitive.  The answer is in lowercase.
+"""
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-       
-        paragraph = re.sub(r'[^\w\s]',' ',paragraph).lower()
-        paragraph = re.sub(' +', ' ', paragraph)
-        d = collections.Counter(paragraph.split(" "))
         
-        for word in banned:
-            if d.get(word):
-                d.pop(word)
+        wordsDict = {}
+        words = re.sub(r'[^\w]',' ',paragraph.lower()).split()
+
+        for word in words:
+            
+            if word not in banned:
+                wordsDict[word] = wordsDict.get(word, 0) + 1
         
-        return max(dict(d).items(), key=operator.itemgetter(1))[0]
-        
+        return max(wordsDict.items(), key=operator.itemgetter(1))[0]
