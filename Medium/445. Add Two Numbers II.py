b87f91b31b3b6b7b2bb2200112fff7d238d3174f
Khushboo Mehta <khushboomehta1995@gmail.com>
2020-11-07 21:56:35 -0500

    Solve using stack in O(n) time

--- Medium/445. Add Two Numbers II.py
+++ Medium/445. Add Two Numbers II.py
@@ -1,22 +1,16 @@
-"""
-https://leetcode.com/problems/add-two-numbers-ii/
+# https://leetcode.com/problems/add-two-numbers-ii/
+    
+# You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
 
-You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+# Follow up:
+# What if you cannot modify the input lists? In other words, reversing the lists is not allowed.
 
-Follow up:
-What if you cannot modify the input lists? In other words, reversing the lists is not allowed.
+# Example:
 
-Example:
-
-Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
-Output: 7 -> 8 -> 0 -> 7
-
-Method1: iterate both LL and store values in stack. Pop them while adding the number and storing it to new LL
-Method2: iterate both LL and generate num = num*10 + ll.val. Add those numbers, convert result to list and add elements of list to string.
-
-"""
+# Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 8 -> 0 -> 7
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -27,36 +21,23 @@ Method2: iterate both LL and generate num = num*10 + ll.val. Add those numbers, 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         
-        def reverse(ll: ListNode):
-            res = None
-            while ll:
-                tem = ll
-                ll = ll.next
-                tem.next = res
-                res = tem
-            return res
+        stack1 , stack2 = [], []
+        while l1:
+            stack1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            stack2.append(l2.val)
+            l2 = l2.next
         
-        l1 = reverse(l1)
-        l2 = reverse(l2)
-
-        temp = ListNode(0)
-        result = temp
-        quotient = 0
-        
-        while l1 or l2 or quotient == 1:
-            x, y = 0, 0
-            
-            if l1:
-                x = l1.val
-                l1 = l1.next
-            if l2:
-                y = l2.val
-                l2 = l2.next
-                
-            num = x + y + quotient
-            temp.next = ListNode(num % 10)
-            temp = temp.next
-            quotient = num // 10
-        
-            
-        return reverse(result.next)
+        carry, result = 0, None
+        while stack1 or stack2 or carry:
+            x = y = 0
+            if stack1:
+                x = stack1.pop()
+            if stack2:
+                y = stack2.pop()
+            num = x + y + carry
+            carry = num // 10
+            node = ListNode(num % 10, result)
+            result = node
+        return result
